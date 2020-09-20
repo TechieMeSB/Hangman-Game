68e07a3f1e55f264b05b6b37ac20b1f9ecd3605f
SriB10 <71335691+SriB10@users.noreply.github.com>
2020-09-20 18:07:10 +0530

    Add files via upload

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from "react";
+
+const Header = () => {
+  return (
+    <Fragment>
+      <h1>Hangman</h1>
+      <p>Find the word </p>
+      <p>Enter a letter and guess the word</p>
+    </Fragment>
+  );
+};
+
+export default Header;
